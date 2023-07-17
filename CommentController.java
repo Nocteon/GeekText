996fdbacb0aa1e92eb49a3caec3b7b4401f8476c
Jonahys09 <134012444+Jonahys09@users.noreply.github.com>
2023-07-17 18:33:48 -0400

    Create CommentController.java

---
+++ CommentController.java
@@ -0,0 +1,30 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+@RestController
+@RequestMapping(value = "/comments")
+public class CommentController {
+    Scanner scanner = new Scanner(System.in);
+
+    @PostMapping
+    public ResponseEntity<?> createComment() {
+        System.out.print("Enter your User ID: ");
+        String userId = scanner.nextLine();
+
+        System.out.print("Enter the Book ID: ");
+        String bookId = scanner.nextLine();
+
+        System.out.print("Enter your comment: ");
+        String comment = scanner.nextLine();
+
+        String message = "Comment created successfully";
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
+
+}
