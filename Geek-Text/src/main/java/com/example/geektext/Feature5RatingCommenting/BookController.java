734c878d5d7e82278bd6a265c78a87a2d1747ffe
Jonahys Camero <134012444+Jonahys09@users.noreply.github.com>
2023-07-25 20:27:05 -0400

    Update BookController.java

--- Geek-Text/src/main/java/com/example/geektext/Feature5RatingCommenting/BookController.java
+++ Geek-Text/src/main/java/com/example/geektext/Feature5RatingCommenting/BookController.java
@@ -1,1 +1,25 @@
 
+package com.geektext.app.rest.Controllers;
+
+import com.geektext.app.rest.Models.Book;
+import com.geektext.app.rest.Repo.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepo bookRepo;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addBook(@RequestParam Book book) {
+        Long id;
+
+        bookRepo.save(book);
+        return "Book added successfully!";
+    }
+}
