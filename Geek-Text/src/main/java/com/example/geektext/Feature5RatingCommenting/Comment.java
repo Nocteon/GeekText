96963dc103aead38882fc975162da1b202e28338
Jonahys Camero <134012444+Jonahys09@users.noreply.github.com>
2023-07-25 20:28:19 -0400

    Update Comment.java

--- Geek-Text/src/main/java/com/example/geektext/Feature5RatingCommenting/Comment.java
+++ Geek-Text/src/main/java/com/example/geektext/Feature5RatingCommenting/Comment.java
@@ -1,1 +1,67 @@
+package com.geektext.app.rest.Models;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private Long userId;
+
+    private Long bookId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date datestamp;
+
+
+    // Constructors
+    public Comment() {
+        // Default constructor
+    }
+
+    public Comment(String content, Date datestamp) {
+        this.content = content;
+        this.datestamp = datestamp;
+    }
+
+
+    // Getters and setters
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDatestamp() {
+        return datestamp;
+    }
+
+    public void setDatestamp(Date datestamp) {
+        this.datestamp = datestamp;
+    }
+}
