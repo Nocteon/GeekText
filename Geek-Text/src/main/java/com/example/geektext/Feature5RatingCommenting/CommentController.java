3d80088ecf89ad0894e4ba5895e58c008630422d
Jonahys Camero <134012444+Jonahys09@users.noreply.github.com>
2023-07-25 20:25:15 -0400

    Update CommentController.java

--- Geek-Text/src/main/java/com/example/geektext/Feature5RatingCommenting/CommentController.java
+++ Geek-Text/src/main/java/com/example/geektext/Feature5RatingCommenting/CommentController.java
@@ -1,32 +1,46 @@
-package com.example.geektext.Feature5RatingCommenting;
+package com.geektext.app.rest.Controllers;
 
+import com.geektext.app.rest.Models.Comment;
+import com.geektext.app.rest.Repo.CommentRepo;
+import com.geektext.app.rest.Requests.CommentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.Date;
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/comments")
+@RequestMapping("/comments")
 public class CommentController {
-    Scanner scanner = new Scanner(System.in);
+
+    @Autowired
+    private CommentRepo commentRepo;
 
     @PostMapping
-    public ResponseEntity<?> createComment() {
-        System.out.print("Enter your User ID: ");
-        String userId = scanner.nextLine();
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createComment(@RequestBody CommentRequest commentRequest) {
+        // Get the user id and book id from the request
+        Long userId = commentRequest.getUserId();
+        Long bookId = commentRequest.getBookId();
+        String comment = commentRequest.getComment();
 
-        System.out.print("Enter the Book ID: ");
-        String bookId = scanner.nextLine();
 
-        System.out.print("Enter your comment: ");
-        String comment = scanner.nextLine();
+        // Create a new Comment object
+        Comment content = new Comment(comment, new Date());
 
-        String message = "Comment created successfully";
-        return new ResponseEntity<>(message, HttpStatus.CREATED);
+        // Save the comment to the database
+        commentRepo.save(content);
+
+        return "Comment saved";
     }
 
+
+    @GetMapping("/{bookId}")
+    public List<Comment> getCommentsForBook(@RequestParam Long bookId) {
+
+        List<Comment> comments = commentRepo.findByBookId(bookId);
+
+        return comments;
+    }
 }
